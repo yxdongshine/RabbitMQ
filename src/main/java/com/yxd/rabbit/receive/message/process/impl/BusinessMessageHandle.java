b4719e24830fe6e913b9619696477d02899bf77f
yxdshine <659777399@qq.com>
2017-11-02 15:24:58 +0800

    mq异步消息处理：消费者异步接受 架构                                                 测试完成

--- src/main/java/com/yxd/rabbit/receive/message/process/impl/BusinessMessageHandle.java
+++ src/main/java/com/yxd/rabbit/receive/message/process/impl/BusinessMessageHandle.java
@@ -3,12 +3,18 @@ package com.yxd.rabbit.receive.message.process.impl;
 import com.yxd.rabbit.body.Message;
 import com.yxd.rabbit.receive.message.process.factory.CurrencyMessageHandle;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by YXD on 2017/10/31.
  */
 public class BusinessMessageHandle extends CurrencyMessageHandle{
     @Override
     public void doMessage(Message message) {
-        System.out.println(":"+message);
+        try {
+            System.out.println(new String(message.getB(),"utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 }
